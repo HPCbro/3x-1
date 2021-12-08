44a83fe6c26ca00fa7075cad8fa848afa22ff613
HPCbro <43901518+HPCbro@users.noreply.github.com>
2021-12-08 12:25:34 +0200

    Create Core.py

---
+++ Core.py
@@ -0,0 +1,15 @@
+f = open("dfsdfsfddf.txt",'a')
+m = int(input("min:"))
+n = int(input("max:"))
+x = 0
+for x in range(m,n):
+    print(str(x)+"/"+str(n))
+    f.write("start:"+str(x)+"\n")
+    while x > 1:
+        if x % 2 == 0:
+            x = x/2
+            f.write(str(x)+"\n")
+        else:
+            x = x * 3 + 1
+            f.write(str(x)+"\n")
+print("The end)")
